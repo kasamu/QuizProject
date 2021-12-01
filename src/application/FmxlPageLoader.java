427612b2633e3848d88c77adaed426b79846cf3c
Kabir <kasamu@unb.ca>
2021-12-01 10:04:55 -0400

    Fixed the forward and backward button and ensure that the application is a single stage.

--- src/application/FmxlPageLoader.java
+++ src/application/FmxlPageLoader.java
@@ -7,10 +7,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public final class FmxlPageLoader {
+public class FmxlPageLoader {
 	
-	//set file location url;
+	//declare file location url name history;
+	private static DLLFileUrl fileHistory;
+	
+	//declare file location url name
 	private static String fileUrl;
+	
+	//declaring a loader class
+	private static FXMLLoader loader;
 	
 	//declare root file
 	private static Parent root;
@@ -21,16 +27,51 @@ public final class FmxlPageLoader {
 	//declare the stage
 	private static Stage primaryStage;
 	
+	//declaring the back form
+	private static String backFile = null;
+	
+	//declaring the forward form;
+	private static String forwardFile = null;
+	
+	
+	public FmxlPageLoader(String inFileUrl) {
+		
+		insertInFileHistory(inFileUrl);
+		
+		setFileUrl();
+		
+		init();
+		
+	}
+	
+	public FmxlPageLoader(Stage stageIn) {
+		
+		fileUrl = "HomeView.fxml";
+		
+		createFileHistory("HomeView.fxml");
+		
+		primaryStage = stageIn;
+		
+		init();
+		
+	}
 	
 	public FmxlPageLoader() {
 		
-		fileUrl = "HomeView.fxml";
+	}
+	
+	public void show() {
+		
+		// Show the primary Stage (window)
+		primaryStage.show();
+		
+	}
+	
+	public void init() {
 		
 		setRoot();
 		
 		setScene();
-		
-		setPrimaryStage();
 		
 	}
 	
@@ -45,8 +86,9 @@ public final class FmxlPageLoader {
 		
 		try {
 			
-			root = FXMLLoader.load(getClass().getResource("/UserInterface/" + fileUrl));
-		
+			setLoader();
+			root = FmxlPageLoader.loader.load();
+			
 		}catch (IOException e) {
 			
 			System.out.println("THE ROOT URL IS NOT FUCTIONING PROPERLY");
@@ -56,9 +98,24 @@ public final class FmxlPageLoader {
 		
 	}
 		
-	public void setFileUrl(String fileUrl) {
-			
-		FmxlPageLoader.fileUrl = fileUrl;
+	public void setFileUrl(String infileUrl) {
+		
+		FmxlPageLoader.fileUrl = infileUrl;
+		
+	}
+	
+	public void setFileUrl() {
+		
+		FmxlPageLoader.fileUrl = FmxlPageLoader.fileHistory.getCurrentNode().getFileUrl();
+		
+	}
+	
+	public String getFileUrl() {
+		
+		setBackFile();
+		setForwardFile();
+		return fileUrl;
+	
 	}
 	
 	public Parent getRoot() {
@@ -80,5 +137,94 @@ public final class FmxlPageLoader {
 		
 		// JavaFX  Scene (window content) inside a primaryStage (window)
 		primaryStage.setScene(FmxlPageLoader.scene);
+		primaryStage.setTitle("Quiz Trivial System");
+		System.out.println("Complete.");
+		getFileHistory().display();
 	}
+
+	public FXMLLoader getLoader() {
+		
+		return loader;
+	}
+
+	public void setLoader() {
+		
+		FmxlPageLoader.loader = new FXMLLoader();
+		FmxlPageLoader.loader.setLocation((getClass().getResource("/UserInterface/" + getFileUrl())));
+		System.out.println("Loading Page ....");
+		
+	}
+	
+	
+	public void getBackFile() {
+		
+		if(backFile != null) {
+			getFileHistory().setCurrentNode(fileHistory.getCurrentNode().getPrevious());
+			setFileUrl(backFile);
+			System.out.println("BackFile: " + backFile);
+			
+			init();
+		}
+		
+		
+	}
+
+	public void setBackFile() {
+		
+		if(getFileHistory().getCurrentNode().getPrevious() != null)
+			FmxlPageLoader.backFile = getFileHistory().getCurrentNode().getPrevious().getFileUrl();
+		
+		return;
+		
+	}
+
+	public void getForwardFile() {
+		
+		if(forwardFile == getFileHistory().getCurrentNode().getFileUrl())
+			forwardFile = null;
+		
+		if(forwardFile != null) {
+			getFileHistory().setCurrentNode(fileHistory.getCurrentNode().getNext());
+			setFileUrl(forwardFile);
+			System.out.println("forwardFile: " + forwardFile);
+			
+		}
+		
+		
+		
+		init();
+		
+		
+	}
+
+	public void setForwardFile() {
+		
+		if(getFileHistory().getCurrentNode().getNext() != null)
+			FmxlPageLoader.forwardFile = getFileHistory().getCurrentNode().getNext().getFileUrl();
+		
+		return;
+		
+	}
+
+	/**
+	 * @return the fileHistory
+	 */
+	public DLLFileUrl getFileHistory() {
+		
+		return FmxlPageLoader.fileHistory;
+	
+	}
+
+	
+	public void createFileHistory(String inFileUrl) {
+		
+		FmxlPageLoader.fileHistory = new DLLFileUrl(inFileUrl);
+		
+	}
+	
+	public void insertInFileHistory(String inFileUrl) {
+		
+		FmxlPageLoader.fileHistory.insert(inFileUrl);
+	}
+
 }
